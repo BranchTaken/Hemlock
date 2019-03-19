0a7e4c99228ab1ba9436ac1e68315ad1ef5d236f
Jason Evans <jasone@canonware.com>
2019-03-19 08:25:41 -0700

    Test Unit.
    This resolves #1.

--- bootstrap/src/unit.ml
+++ bootstrap/src/unit.ml
@@ -30,4 +30,37 @@ include Identifiable.Make(T)
  * Begin tests.
  *)
 
-(* XXX Add tests. *)
+let%expect_test "string" =
+  let open Printf in
+  printf "to_string () -> %s\n" (to_string ());
+  printf "of_string unit -> %s\n" (to_string (of_string "unit"));
+  printf "of_string () -> %s\n" (to_string (of_string "()"));
+
+  [%expect{|
+    to_string () -> ()
+    of_string unit -> ()
+    of_string () -> ()
+    |}]
+
+let%expect_test "sexp" =
+  let open Printf in
+  let sexp = sexp_of_t () in
+  printf "sexp_of_t %s -> %s ; " (to_string ()) (Sexplib.Sexp.to_string sexp);
+  printf "t_of_sexp -> %s\n" (to_string (t_of_sexp sexp));
+
+  [%expect{| sexp_of_t () -> () ; t_of_sexp -> () |}]
+
+let%expect_test "eq" =
+  let open Printf in
+  let t0 = () in
+  let t1 = () in
+  printf "cmp %s %s -> %s\n" (to_string t0) (to_string t1)
+    (Sexplib.Sexp.to_string (Cmp.sexp_of_t (cmp t0 t1)));
+  printf "%s = %s -> %B\n" (to_string t0) (to_string t1) (t0 = t1);
+  printf "%s <> %s -> %B\n" (to_string t0) (to_string t1) (t0 <> t1);
+
+  [%expect{|
+    cmp () () -> Eq
+    () = () -> true
+    () <> () -> false
+    |}]
