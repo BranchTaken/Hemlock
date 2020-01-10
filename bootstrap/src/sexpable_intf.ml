72f0452414dea330367ae6388cf052409974f725
Jason Evans <jasone@canonware.com>
2020-01-10 13:03:15 -0800

    Document Sexpable.

--- bootstrap/src/sexpable_intf.ml
+++ bootstrap/src/sexpable_intf.ml
@@ -1,5 +1,10 @@
+(** Sexp conversion functions. *)
 module type S = sig
   type t
+
   val t_of_sexp: Sexplib0.Sexp.t -> t
+  (** Convert {!type:Sexp.t} to [t]. *)
+
   val sexp_of_t: t -> Sexplib0.Sexp.t
+  (** Convert [t] to {!type:Sexp.t}. *)
 end
