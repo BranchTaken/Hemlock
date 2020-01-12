ad7a961f338ea8113550c37c58d40e0e9259013b
Jason Evans <jasone@canonware.com>
2020-01-11 22:08:07 -0800

    Document Bool.

--- bootstrap/src/bool.mli
+++ bootstrap/src/bool.mli
@@ -1,9 +1,12 @@
+(** Boolean type. *)
+
 type t = bool
 
 include Identifiable_intf.S with type t := t
 include Intable_intf.S with type t := t
 
 val not: t -> t
+(** [not t] returns the logical negation of [t]. *)
 
 (* The && and || operators built in to OCaml are magically short-circuiting.
  * If we define equivalent operators they lose the short-circuiting magic. *)
