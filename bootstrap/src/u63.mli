148a5a328ec17c3259cd115f3f6a5d4014f8b5e7
Jason Evans <jasone@canonware.com>
2020-01-11 22:08:55 -0800

    Document U63.

--- bootstrap/src/u63.mli
+++ bootstrap/src/u63.mli
@@ -1,8 +1,15 @@
+(** 63-bit unsigned integer type. *)
+
 open Rudiments_uint
 
 type t = uint
 include Intnb_intf.S_u with type t := t
 
 val to_int: t -> int
+(** Convert an unsigned integer to a bitwise identical signed integer. *)
+
 val of_int: int -> t
+(** Convert an unsigned integer to a bitwise identical signed integer. *)
+
 val kv: int -> t
+(** Create a constant value. *)
