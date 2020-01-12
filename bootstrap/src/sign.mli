ef7303efdf3bc564623676e59867d4ede4d353e2
Jason Evans <jasone@canonware.com>
2020-01-11 22:09:19 -0800

    Document Sign.

--- bootstrap/src/sign.mli
+++ bootstrap/src/sign.mli
@@ -1,13 +1,24 @@
+(** Sign type. *)
+
 type t =
-| Neg
-| Zero
-| Pos
+| Neg  (** Negative. *)
+| Zero (** Zero. *)
+| Pos  (** Positive. *)
 
 include Sexpable_intf.S with type t := t
 
 val of_int: int -> t
+(** [of_int x] returns [Neg] if [x < 0], [Zero] if [x = 0], or [Pos] if [x > 0].
+    *)
+
 val to_int: t -> int
+(** [to_int t] returns [-1], [0], or [1]. *)
+
 val to_float: t -> float
+(** [to_int t] returns [-1.], [0.], or [1.]. *)
 
 val flip: t -> t
+(** [flip t] negates [t]. *)
+
 val ( * ): t -> t -> t
+(** [t0 * t1] multiplies the signs; e.g. [Neg * Neg] returns [Pos]. *)
