de13d11c309a0f5c57d1cae92ed0f31ed1ebf644
Jason Evans <jasone@canonware.com>
2020-01-21 12:10:05 -0800

    Fix a doc typo.

--- bootstrap/src/formattable_intf.ml
+++ bootstrap/src/formattable_intf.ml
@@ -27,7 +27,7 @@ module type S_poly2 = sig
 
   val pp: (Format.formatter -> 'a -> unit) -> (Format.formatter -> 'b -> unit)
     -> Format.formatter -> ('a, 'b) t -> unit
-  (** [pp pp_a bb_b ppf t] prints a syntactic representation of [t] to the
+  (** [pp pp_a pp_b ppf t] prints a syntactic representation of [t] to the
       pretty printing formatter, [ppf], using the [pp_a] printer for the
       parametric type value [a], and [pp_b] for the parametric type value [b].
       This function is intended for use with the [%a] format specifier to
