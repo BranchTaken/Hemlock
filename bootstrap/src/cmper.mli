29a0e047a80eb522b6700f7bab568b97f60eabe4
Jason Evans <jasone@canonware.com>
2020-02-06 22:31:27 -0800

    Add hash_fold to Cmper.t .

--- bootstrap/src/cmper.mli
+++ bootstrap/src/cmper.mli
@@ -4,6 +4,9 @@
     that multi-container operations can only be performed on containers with
     compatible comparison functions. *)
 type ('a, 'witness) t = private {
+  hash_fold: 'a -> Hash.State.t -> Hash.State.t;
+  (** Hash-fold function. *)
+
   cmp: 'a -> 'a -> Cmp.t;
   (** Comparison function. *)
 
@@ -15,6 +18,7 @@ type ('a, 'witness) t = private {
     {!type:string}. *)
 module type I_mono = sig
   type t
+  include Hashable_intf.Key with type t := t
   include Cmpable_intf.I_mono with type t := t
   include Formattable_intf.S_mono with type t := t
 end
@@ -46,6 +50,18 @@ module Make_mono (T : I_mono) : S_mono with type t := T.t
     {!type:'a list}. *)
 module type I_poly = sig
   type 'a t
+  val hash_fold: ('a -> Hash.State.t -> Hash.State.t) -> 'a t -> Hash.State.t
+    -> Hash.State.t
+  (** [hash_fold hash_fold_a t state] incorporates the hash of [t] into [state]
+      and returns the resulting state.  Container elements are sequentially
+      hash-folded into the resulting state via [hash_fold_a]. *)
+
+  val hash_fold_a: 'a -> Hash.State.t -> Hash.State.t
+  (** [hash_fold_a a state] incorporates the hash of [a] into [state] and
+      returns the resulting state.  {!Make_poly} synthesizes a monomorphic
+      [hash_fold] from the composition of [hash_fold] and [hash_fold_a].
+      [hash_fold_a] is the hash-fold function for {!type:'a}. *)
+
   include Cmpable_intf.I_poly with type 'a t := 'a t
   include Formattable_intf.S_poly with type 'a t := 'a t
 
@@ -74,5 +90,7 @@ end
     resulting module contains a comparator that can be used in conjunction with
     e.g. {!type:OrdMap}.  The comparator witness assures that multi-container
     operations can only be performed on containers with compatible comparison
-    functions. *)
+    functions.  This functor is unusual in that it creates a monomorphic
+    wrapper around a specific actualization of a polymorphic type, so that
+    comparators are always in effect monomorphic. *)
 module Make_poly (T : I_poly) : S_poly with type 'a t := 'a T.t
