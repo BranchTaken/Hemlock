BranchTaken/Hemlock

commit dfdaa2384213394ea3818e717c0f9e5d3eacd85d

Rename uint/int to usize/isize, and make usize the default integer type.
Prior to this change, Hemlock's int was explicitly equivalent to OCaml's int.
Now Hemlock's usize is explicitly equivalent to OCaml's int, and isize is
opaque.  This means that usize is now the Hemlock integer type which can be
created via literal constants and matched with literal constants; isize now
requires Isize.kv for constant creation, and extra work to deal with in pattern
matches.

Care is required in the narrow unsigned integer modules (U21 and U8), because
within those modules the compiler knows that e.g. u21 and usize are equivalent
types.  Thus it is possible to create out-of-range constants if kv calls are
omitted.

Remove the Intable interface.  It was used only by Bool and Float.  Bool now
has {to,of}_usize, and Float now has {to,of}_isize.

Fix integer printing and to_string for narrow integers by consolidating pp,
pp_x, and to_string into Intnb.Make_common.

bootstrap/bin/hlc.ml
@@ -5,5 +5,5 @@ type 'a v = 'a array
 
 let _ =
   let x:(int v) = Array.of_list [1; 2; 3] in
-  Printf.printf "Array.length x -> %d\n" (Uint.to_int (Array.length x));
+  Printf.printf "Array.length x -> %d\n" (Usize.to_int (Array.length x));
   1

bootstrap/src/array.mli
@@ -55,16 +55,16 @@ module Seq : sig
      and type 'a elm := 'a T.elm
 end
 
-val init: uint -> f:(uint -> 'a) -> 'a t
+val init: usize -> f:(usize -> 'a) -> 'a t
 (** Initialize array.  [init len ~f:(fun i -> ...)] initializes an array of
     given length, where [f] provides the value for each element at given index.
 *)
 
-val of_list: ?length:uint -> 'a list -> 'a t
+val of_list: ?length:usize -> 'a list -> 'a t
 (** Initialize array using contents of list.  If specified, [?length] must equal
     [(List.length list)]. *)
 
-val of_list_rev: ?length:uint -> 'a list -> 'a t
+val of_list_rev: ?length:usize -> 'a list -> 'a t
 (** Initialize array using reversed contents of list.  If specified, [?length]
     must equal [(List.length list)]. *)
 
@@ -73,20 +73,20 @@ val to_list: 'a t -> 'a list
 val to_list_rev: 'a t -> 'a list
 *)
 
-val length: 'a t -> uint
+val length: 'a t -> usize
 (** Return array length. *)
 
 val is_empty: 'a t -> bool
 (** Return [true] if array length is 0; [false] otherwise. *)
 
-val get: 'a t -> uint -> 'a
+val get: 'a t -> usize -> 'a
 (** Get array element.  [get t i] returns the element at index [i]. *)
 
-val set_inplace: 'a t -> uint -> 'a -> unit
+val set_inplace: 'a t -> usize -> 'a -> unit
 (** Set array element in place (mutate).  [set_inplace t i elm] sets the element
     at index [i] to [elm]. *)
 
-val set: 'a t -> uint -> 'a -> 'a t
+val set: 'a t -> usize -> 'a -> 'a t
 (** Create a new array based on input array, differing in one element.
     [set t i elm] creates an array equal to [t], except that element [i] is
     initialized to [elm]. *)
@@ -94,7 +94,7 @@ val set: 'a t -> uint -> 'a -> 'a t
 val copy: 'a t -> 'a t
 (** Create a copy of an array. *)
 
-val pare: 'a t -> base:uint -> past:uint -> 'a t
+val pare: 'a t -> base:usize -> past:usize -> 'a t
 (** Create an array with contents initialized to equal the \[[base]..[past])
     input subarray. *)
 
@@ -112,11 +112,11 @@ val prepend: 'a -> 'a t -> 'a t
 (** Create an array that is the concatenation of the input element and the input
     array. *)
 
-val insert: 'a t -> uint -> 'a -> 'a t
+val insert: 'a t -> usize -> 'a -> 'a t
 (** Create an array that is the concatenation of the bipartition of the input
     array at specified index, with the input element interposed. *)
 
-val remove: 'a t -> uint -> 'a t
+val remove: 'a t -> usize -> 'a t
 (** Create an array that is the concatenation of the first and third components
     of the tripartition about the element at specified index. *)
 
@@ -130,10 +130,10 @@ val reduce_hlt: 'a t -> f:('a -> 'a -> 'a) -> 'a
     reduction function is assumed to be associative; thus reduction order is
     unspecified. *)
 
-val swap_inplace: 'a t -> uint -> uint -> unit
+val swap_inplace: 'a t -> usize -> usize -> unit
 (** Swap elements at given indices in place (mutate). *)
 
-val swap: 'a t -> uint -> uint -> 'a t
+val swap: 'a t -> usize -> usize -> 'a t
 (** Create an array based on the input array, but with elements at given indices
     swapped. *)
 
@@ -143,7 +143,7 @@ val rev_inplace: 'a t -> unit
 val rev: 'a t -> 'a t
 (** Create an array with contents reversed relative to the input array. *)
 
-val blit: 'a t -> uint -> 'a t -> uint -> uint -> unit
+val blit: 'a t -> usize -> 'a t -> usize -> usize -> unit
 (** Set a range of elements in place (mutate).  [blit t0 i0 t1 i1 len] sets the
     elements of [t1] at [\[i1..i1+len)] to equal the elements of [t0] at
     [\[i0..i0+len)].  Overlapping ranges are supported: [t0] and [t1] may refer
@@ -163,8 +163,8 @@ val sort: ?stable:bool -> 'a t -> cmp:('a -> 'a -> Cmp.t) -> 'a t
     comparison function.  Preserve order of equivalent elements if [?stable] is
     [true]. *)
 
-val psearch: ?base:uint -> ?past:uint -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
-  -> (Cmp.t * uint) option
+val psearch: ?base:usize -> ?past:usize -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
+  -> (Cmp.t * usize) option
 (** Binary search for key in array, selecting the leftmost match, and falling
     back to the predecessor in the case of no match.
     @return {ul
@@ -175,20 +175,20 @@ val psearch: ?base:uint -> ?past:uint -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
     }
 *)
 
-val search: ?base:uint -> ?past:uint -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
-  -> uint option
+val search: ?base:usize -> ?past:usize -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
+  -> usize option
 (** Binary search for key in array.  If key is found, return [(Some index)],
     otherwise return [None].  Note that if more than one element matches key, an
     arbitrary match is returned. *)
 
-val nsearch: ?base:uint -> ?past:uint -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
-  -> (Cmp.t * uint) option
+val nsearch: ?base:usize -> ?past:usize -> 'a t -> 'a -> cmp:('a -> 'a -> Cmp.t)
+  -> (Cmp.t * usize) option
 (** Binary search for key in array, selecting the rightmost match, and falling
     back to the successor.
     @return {ul
       {- Successor: [Some (Cmp.Lt, index)]}
       {- Rightmost match: [Some (Cmp.Eq, index)]}
-      {- No successor: [Some (Cmp.Gt, (Uint.pred (length t)))]}
+      {- No successor: [Some (Cmp.Gt, (Usize.pred (length t)))]}
       {- Empty array: [None]}
     }
 *)
@@ -197,7 +197,7 @@ val map: 'a t -> f:('a -> 'b) -> 'b t
 (** Create an array with elements mapped from the input array, according to the
     element mapping function. *)
 
-val mapi: 'a t -> f:(uint -> 'a -> 'b) -> 'b t
+val mapi: 'a t -> f:(usize -> 'a -> 'b) -> 'b t
 (** Create an array with elements mapped from the input array, according to the
     indexed element mapping function. *)
 
@@ -206,7 +206,7 @@ val fold_map: 'a t -> init:'accum -> f:('accum -> 'a -> 'accum * 'b)
 (** Create an array and accumulated result with elements mapped from the input
     array, according to the folding mapping function. *)
 
-val foldi_map: 'a t -> init:'accum -> f:(uint -> 'accum -> 'a -> 'accum * 'b)
+val foldi_map: 'a t -> init:'accum -> f:(usize -> 'accum -> 'a -> 'accum * 'b)
   -> 'accum * 'b t
 (** Create an array and accumulated result with elements mapped from the input
     array, according to the indexed folding mapping function. *)
@@ -216,7 +216,7 @@ val filter: 'a t -> f:('a -> bool) -> 'a t
     for which the filter function returns true are incorporated in the result.
 *)
 
-val filteri: 'a t -> f:(uint -> 'a -> bool) -> 'a t
+val filteri: 'a t -> f:(usize -> 'a -> bool) -> 'a t
 (** Create an array with contents filtered by the given function.  Only elements
     for which the indexed filter function returns true are incorporated in the
     result. *)
@@ -228,7 +228,7 @@ val fold2_until: 'a t -> 'b t -> init:'accum
     folding early if the folding function returns true. *)
 
 val foldi2_until: 'a t -> 'b t -> init:'accum
-  -> f:(uint -> 'accum -> 'a -> 'b -> 'accum * bool) -> 'accum
+  -> f:(usize -> 'accum -> 'a -> 'b -> 'accum * bool) -> 'accum
 (** Create an accumulated result for the paired elements of two arrays, calling
     the indexed element folding function in increasing index order, and
     terminate folding early if the folding function returns true. *)
@@ -239,7 +239,7 @@ val fold2: 'a t -> 'b t -> init:'accum -> f:('accum -> 'a -> 'b -> 'accum)
     the element folding function in increasing index order. *)
 
 val foldi2: 'a t -> 'b t -> init:'accum
-  -> f:(uint -> 'accum -> 'a -> 'b -> 'accum) -> 'accum
+  -> f:(usize -> 'accum -> 'a -> 'b -> 'accum) -> 'accum
 (** Create an accumulated result for the paired elements of two arrays, calling
     the indexed element folding function in increasing index order. *)
 
@@ -247,7 +247,7 @@ val iter2: 'a t -> 'b t -> f:('a -> 'b -> unit) -> unit
 (** Iterate over the paired elements of two arrays, calling the element visiting
     function in increasing index order. *)
 
-val iteri2: 'a t -> 'b t -> f:(uint -> 'a -> 'b -> unit) -> unit
+val iteri2: 'a t -> 'b t -> f:(usize -> 'a -> 'b -> unit) -> unit
 (** Iterate over the paired elements of two arrays, calling the indexed element
     visiting function in increasing index order. *)
 
@@ -255,7 +255,7 @@ val map2: 'a t -> 'b t -> f:('a -> 'b -> 'c) -> 'c t
 (** Create an array with elements mapped by the element mapping function from
     the paired elements of two input arrays. *)
 
-val mapi2: 'a t -> 'b t -> f:(uint -> 'a -> 'b -> 'c) -> 'c t
+val mapi2: 'a t -> 'b t -> f:(usize -> 'a -> 'b -> 'c) -> 'c t
 (** Create an array with elements mapped by the indexed element mapping function
     from the paired elements of two input arrays. *)
 
@@ -266,7 +266,7 @@ val fold2_map: 'a t -> 'b t -> init:'accum
     order. *)
 
 val foldi2_map: 'a t -> 'b t -> init:'accum
-  -> f:(uint -> 'accum -> 'a -> 'b -> 'accum * 'c) -> 'accum * 'c t
+  -> f:(usize -> 'accum -> 'a -> 'b -> 'accum * 'c) -> 'accum * 'c t
 (** Create an array and accumulated result based on the paired elements of two
     arrays, calling the indexed element folding/mapping function in increasing
     index order. *)

bootstrap/src/bool.ml
@@ -33,12 +33,12 @@ end
 include T
 include Identifiable.Make(T)
 
-let of_int x =
+let of_usize x =
   match x with
   | 0 -> false
   | _ -> true
 
-let to_int t =
+let to_usize t =
   match t with
   | false -> 0
   | true -> 1
@@ -53,21 +53,21 @@ let not t =
  *)
 
 let%expect_test "int" =
-  let open Printf in
+  let open Format in
   let rec fn = function
     | [] -> ()
     | t :: ts' -> begin
-      let x = to_int t in
-      printf "to_int %b -> %d ; " t x;
-      printf "of_int %d -> %b\n" x (of_int x);
+      let x = to_usize t in
+      printf "to_usize %b -> %a ; " t Usize.pp x;
+      printf "of_usize %a -> %b\n" Usize.pp x (of_usize x);
       fn ts'
     end
   in
   fn [false; true];
 
   [%expect{|
-    to_int false -> 0 ; of_int 0 -> false
-    to_int true -> 1 ; of_int 1 -> true
+    to_usize false -> 0 ; of_usize 0 -> false
+    to_usize true -> 1 ; of_usize 1 -> true
     |}]
 
 let%expect_test "string" =

bootstrap/src/bool.mli
@@ -1,9 +1,16 @@
 (** Boolean type. *)
 
+open Rudiments
+
 type t = bool
 
 include Identifiable_intf.S with type t := t
-include Intable_intf.S with type t := t
+
+val of_usize: usize -> t
+(** Initialize from unsigned integer. *)
+
+val to_usize: t -> usize
+(** Convert to unsigned integer. *)
 
 val not: t -> t
 (** [not t] returns the logical negation of [t]. *)

bootstrap/src/bytes.ml
@@ -8,7 +8,7 @@ module Array_seq = struct
     type t = {
       string: string;
       cursor: String.Cursor.t;
-      bindex: uint;
+      bindex: usize;
       rem_bytes: byte list;
     }
     type elm = byte
@@ -16,8 +16,8 @@ module Array_seq = struct
     let init t =
       {
           string=t;
-          cursor=(String.Cursor.at t ~bindex:(kv 0));
-          bindex=(kv 0);
+          cursor=(String.Cursor.at t ~bindex:0);
+          bindex=0;
           rem_bytes=[];
       }
 
@@ -25,11 +25,11 @@ module Array_seq = struct
       (String.blength t.string) - t.bindex
 
     let next t =
-      assert (length t > (kv 0));
+      assert (length t > 0);
       match t.rem_bytes with
       | b :: rem_bytes' -> begin
           let t' = {t with
-                    bindex=(Uint.succ t.bindex);
+                    bindex=(Usize.succ t.bindex);
                     rem_bytes=rem_bytes'
                    } in
           b, t'
@@ -43,7 +43,7 @@ module Array_seq = struct
           in
           let t' = {t with
                     cursor=(String.Cursor.succ t.cursor);
-                    bindex=(Uint.succ t.bindex);
+                    bindex=(Usize.succ t.bindex);
                     rem_bytes} in
           b, t'
         end
@@ -59,21 +59,21 @@ module Utf8_seq = struct
   module T = struct
     type t = {
       bytes: byte array;
-      bindex: uint;
+      bindex: usize;
     }
 
     let init t =
-      {bytes=t; bindex=kv 0}
+      {bytes=t; bindex=0}
 
     let length t =
       (Array.length t.bytes) - t.bindex
 
     let next t =
-      match (length t) = (kv 0) with
+      match (length t) = 0 with
       | true -> None
       | false -> begin
           let b = Array.get t.bytes t.bindex in
-          let t' = {t with bindex=(Uint.succ t.bindex)} in
+          let t' = {t with bindex=(Usize.succ t.bindex)} in
           Some (b, t')
         end
   end

bootstrap/src/container_array.ml
@@ -10,7 +10,7 @@ module Make_poly_array (T : I_poly_array) : S_poly_array_gen
     module T = struct
       type 'a t = {
         cursor: 'a T.Cursor.t;
-        length: uint;
+        length: usize;
       }
       type 'a elm = 'a T.elm
 
@@ -24,10 +24,10 @@ module Make_poly_array (T : I_poly_array) : S_poly_array_gen
         t.length
 
       let next t =
-        assert (length t > (kv 0));
+        assert (length t > 0);
         let elm = T.Cursor.rget t.cursor in
         let cursor' = T.Cursor.succ t.cursor in
-        let length' = (Uint.pred t.length) in
+        let length' = (Usize.pred t.length) in
         let t' = {cursor=cursor'; length=length'} in
         elm, t'
     end

bootstrap/src/container_array_intf.ml
@@ -8,7 +8,7 @@ open Container_common_intf
 module type I_poly_array = sig
   include I_poly
 
-  val length: 'a t -> uint
+  val length: 'a t -> usize
   (** Container length. *)
 end
 
@@ -38,7 +38,7 @@ end
 module type I_mono_array = sig
   include I_mono
 
-  val length: t -> uint
+  val length: t -> usize
   (** Container length. *)
 end
 

commit c6d131334a5332cbf1515f7f9147ac950e3846a2

Fix hlc compilation.
Fix hlc.ml to fully adopt uint-to-usize renaming and related API changes.  Also
clean up the code to match its current minimal intent of verifying that an
executable can compile and execute using the bootstrap library code.

This regression was introduced by dfdaa2384213394ea3818e717c0f9e5d3eacd85d
(Rename uint/int to usize/isize, and make usize the default integer type.).

bootstrap/bin/hlc.ml
@@ -1,9 +1,11 @@
 open Hemlock
 include Hemlock.Rudiments
 
-type 'a v = 'a array
-
 let _ =
-  let x:(int v) = Array.of_list [1; 2; 3] in
-  Printf.printf "Array.length x -> %d\n" (Usize.to_int (Array.length x));
-  1
+  let open Format in
+  let x = Array.of_list [1; 2; 3] in
+  printf "@[<h>";
+  printf "Array.length %a -> %a\n"
+    (Array.pp Usize.pp) x
+    Usize.pp (Array.length x);
+  printf "@]"