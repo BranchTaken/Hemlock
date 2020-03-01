66fdb986f99425aed801cc62f8abc1c8354d598f
Jason Evans <jasone@canonware.com>
2020-03-01 12:46:23 -0800

    Add Cmp.is_* convenience functions.
    It is common for code to operate on a comparator rather than having the
    comparison operators of Cmpable.Make available.  These convenience functions
    reduce boilerplate in such cases.

--- bootstrap/src/basis/cmp.mli
+++ bootstrap/src/basis/cmp.mli
@@ -5,3 +5,21 @@ type t =
   | Gt (** Greater than. *)
 
 include Formattable_intf.S_mono with type t := t
+
+val is_ge: t -> bool
+(** [is_ge t] returns [true] if t is [Gt] or [Eq]; false otherwise. *)
+
+val is_le: t -> bool
+(** [is_le t] returns [true] if t is [Lt] or [Eq]; false otherwise. *)
+
+val is_eq: t -> bool
+(** [is_eq t] returns [true] if t is [Eq]; false otherwise. *)
+
+val is_gt: t -> bool
+(** [is_gt t] returns [true] if t is [Gt]; false otherwise. *)
+
+val is_lt: t -> bool
+(** [is_lt t] returns [true] if t is [Lt]; false otherwise. *)
+
+val is_ne: t -> bool
+(** [is_ne t] returns [true] if t is [Lt] or [Gt]; false otherwise. *)
