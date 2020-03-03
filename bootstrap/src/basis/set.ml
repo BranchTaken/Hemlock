1f00568784884c694eee4e98e26864f7b7327c65
Jason Evans <jasone@canonware.com>
2020-03-02 18:25:00 -0800

    Add Set stress test.

--- bootstrap/src/basis/set.ml
+++ bootstrap/src/basis/set.ml
@@ -2721,3 +2721,26 @@ let%expect_test "reduce" =
     reduce ~f:( + ) [42; 420; 421] -> Some 883
     reduce ~f:( + ) [42; 420; 4200] -> Some 4662
     |}]
+
+let%expect_test "stress" =
+  let open Format in
+  printf "@[";
+  (* test is n^2 time complexity, so keep n small. *)
+  let rec test n i e set = begin
+    match i < n with
+    | false -> set
+    | true -> begin
+        let set' = remove i (test n (succ i) e (insert i set)) in
+        assert (equal set set');
+        assert (equal set (union set set'));
+        assert (equal set (inter set set'));
+        assert (equal e (diff set set'));
+        set'
+      end
+  end in
+  let e = empty (module UsizeTestCmper) in
+  let _ = test 100 0 e e in
+  printf "@]";
+
+  [%expect{|
+    |}]
