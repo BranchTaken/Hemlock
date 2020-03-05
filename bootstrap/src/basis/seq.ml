aa1c149b725ee133eb8ceaf2df6ecc0174378b5b
Jason Evans <jasone@canonware.com>
2020-03-05 13:55:50 -0800

    Extract coupled folding from Set into Seq.Make_poly2_fold2 .

--- bootstrap/src/basis/seq.ml
+++ bootstrap/src/basis/seq.ml
@@ -16,3 +16,63 @@ module Make_indef (T : I_mono_indef) : S_mono_indef with type t := T.t
 struct
   include T
 end
+
+module Make_poly2_fold2 (T : I_poly2_fold2) : S_poly2_fold2
+  with type ('a, 'cmp) t := ('a, 'cmp) T.container
+   and type 'a elm := 'a T.elm = struct
+  let fold2_until ~init ~f t0 t1 =
+    let open Cmp in
+    let next seq  = begin
+      match T.next_opt seq with
+      | None -> None, seq
+      | Some (a, seq') -> (Some a), seq'
+    end in
+    let rec fn accum ~f cmp a0_opt seq0 a1_opt seq1 = begin
+      let f_a0_a1 accum ~f cmp a0_opt seq0 a1_opt seq1 = begin
+        let accum', until = f accum a0_opt a1_opt in
+        match until with
+        | true -> accum'
+        | false -> begin
+            let a0_opt', seq0' = next seq0 in
+            let a1_opt', seq1' = next seq1 in
+            fn accum' ~f cmp a0_opt' seq0' a1_opt' seq1'
+          end
+      end in
+      let f_a0 accum ~f cmp a0_opt seq0 a1_opt seq1 = begin
+        let accum', until = f accum a0_opt None in
+        match until with
+        | true -> accum'
+        | false -> begin
+            let a0_opt', seq0' = next seq0 in
+            fn accum' ~f cmp a0_opt' seq0' a1_opt seq1
+          end
+      end in
+      let f_a1 accum ~f cmp a0_opt seq0 a1_opt seq1 = begin
+        let accum', until = f accum None a1_opt in
+        match until with
+        | true -> accum'
+        | false -> begin
+            let a1_opt', seq1' = next seq1 in
+            fn accum' ~f cmp a0_opt seq0 a1_opt' seq1'
+          end
+      end in
+      match a0_opt, a1_opt with
+      | None, None -> accum
+      | Some _, None
+      | None, Some _ -> f_a0_a1 accum ~f cmp a0_opt seq0 a1_opt seq1
+      | Some a0, Some a1 ->
+        match cmp a0 a1 with
+        | Lt -> f_a0 accum ~f cmp a0_opt seq0 a1_opt seq1
+        | Eq -> f_a0_a1 accum ~f cmp a0_opt seq0 a1_opt seq1
+        | Gt -> f_a1 accum ~f cmp a0_opt seq0 a1_opt seq1
+    end in
+    let a0_opt, seq0 = next (T.init t0) in
+    let a1_opt, seq1 = next (T.init t1) in
+    fn init ~f (T.cmp (T.cmper t0)) a0_opt seq0 a1_opt seq1
+
+  let fold2 ~init ~f t0 t1 =
+    fold2_until ~init ~f:(fun accum t0 t1 -> (f accum t0 t1), false) t0 t1
+
+  let iter2 ~f t0 t1 =
+    fold2 ~init:() ~f:(fun _ t0 t1 -> f t0 t1) t0 t1
+end
