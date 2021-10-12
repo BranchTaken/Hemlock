b83502160fce4c13cd6077a018da8fb8ab82ad3a
Jason Evans <jasone@canonware.com>
2021-10-12 13:37:33 -0700

    Print a backtrace when halting.

--- bootstrap/src/basis/rudimentsFunctions.ml
+++ bootstrap/src/basis/rudimentsFunctions.ml
@@ -1,13 +1,17 @@
+let print_error msg =
+  let () = prerr_string (msg ^ "\nat:\n") in
+  prerr_string Printexc.(raw_backtrace_to_string (get_callstack 64))
+
 let not_reached () =
-  let () = prerr_string "Unreachable code reached\n" in
+  let () = print_error "Unreachable code reached" in
   assert false
 
 let not_implemented s =
-  let () = prerr_string ("Not implemented: " ^ s ^ "\n") in
+  let () = print_error ("Not implemented: " ^ s) in
   assert false
 
 let halt s =
-  let () = prerr_string ("Halt: " ^ s ^ "\n") in
+  let () = print_error ("Halt: " ^ s) in
   exit 1
 
 let not t =
