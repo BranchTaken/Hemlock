66685c7eee3110e53bd9c458e10d70eb07cf7886
Jason Evans <jasone@canonware.com>
2021-11-10 15:02:16 -0800

    Refactor Text tests to use Fmt
    Resolves #179.

--- bootstrap/test/basis/text/test_of_bytes_stream.ml
+++ bootstrap/test/basis/text/test_of_bytes_stream.ml
@@ -1,7 +1,6 @@
 open! Basis.Rudiments
 open! Basis
 open Text
-open Format
 
 let stream_of_bytes_list bl =
   Stream.init_indef bl ~f:(fun bl ->
@@ -15,48 +14,48 @@ let stream_of_string_list sl =
     Bytes.Slice.of_string_slice (String.C.Slice.of_string s)))
 
 let test () =
-  printf "@[<h>";
   let fn sl = begin
-    printf "%a\n" (List.xpp String.xpp) sl;
+    File.Fmt.stdout |> (List.pp String.pp) sl |> Fmt.fmt "\n" |> ignore;
     let text = of_bytes_stream (stream_of_string_list sl) in
 
     let rec fwd_iter cursor = begin
       match Cursor.next_opt cursor with
       | None -> cursor
       | Some (cp, cursor') -> begin
-          printf "%s" (Codepoint.to_string cp);
+          File.Fmt.stdout |> Fmt.fmt (Codepoint.to_string cp) |> ignore;
           fwd_iter cursor'
         end
     end in
     let hd = Cursor.hd text in
-    printf "  fwd   -> index=%Lu \"" (Cursor.index hd);
+    File.Fmt.stdout |> Fmt.fmt "  fwd   -> index=" |> Uns.pp (Cursor.index hd) |> Fmt.fmt " \""
+    |> ignore;
     let tl = fwd_iter hd in
-    printf "\"\n";
+    File.Fmt.stdout |> Fmt.fmt "\"\n" |> ignore;
 
     let rec rev_iter cursor = begin
       match Cursor.index cursor > 0L with
       | false -> cursor
       | true -> begin
           let cp, cursor' = Cursor.prev cursor in
-          printf "%s" (Codepoint.to_string cp);
+          File.Fmt.stdout |> Fmt.fmt (Codepoint.to_string cp) |> ignore;
           rev_iter cursor'
         end
     end in
-    printf "  rev   -> index=%Lu \"" (Cursor.index tl);
+    File.Fmt.stdout |> Fmt.fmt "  rev   -> index=" |> Uns.pp (Cursor.index tl) |> Fmt.fmt " \""
+    |> ignore;
     let hd' = rev_iter tl in
-    printf "\"\n";
+    File.Fmt.stdout |> Fmt.fmt "\"\n" |> ignore;
     assert Cursor.(hd text = hd');
 
     let slice =
       Slice.init ~base:(Cursor.hd text) ~past:(Cursor.tl text) text in
     let s' = Slice.to_string slice in
-    printf "  slice -> %a\n" String.xpp s'
+    File.Fmt.stdout |> Fmt.fmt "  slice -> " |> String.pp s' |> Fmt.fmt "\n" |> ignore
   end in
   fn [];
   fn [""];
   fn [""; ""];
   fn ["Hello"];
-  fn ["Hello"; " "; "Goodbye"];
-  printf "@]"
+  fn ["Hello"; " "; "Goodbye"]
 
 let _ = test ()
--- bootstrap/test/basis/text/test_path.ml
+++ bootstrap/test/basis/text/test_path.ml
@@ -1,19 +1,19 @@
 open! Basis.Rudiments
 open! Basis
 open Text
-open Format
 
 let test () =
-  printf "@[<h>";
   let text_path = path in
   let test_path ?path s = begin
     let text = of_string_slice ?path (String.C.Slice.of_string s) in
-    printf "%a -> %a\n"
-      (Option.xpp String.xpp) path
-      (Option.xpp String.xpp) (text_path text)
+    File.Fmt.stdout
+    |> (Option.fmt String.pp) path
+    |> Fmt.fmt " -> "
+    |> (Option.fmt String.pp) (text_path text)
+    |> Fmt.fmt "\n"
+    |> ignore
   end in
   test_path "";
-  test_path ~path:"/foo/bar" "";
-  printf "@]"
+  test_path ~path:"/foo/bar" ""
 
 let _ = test ()
