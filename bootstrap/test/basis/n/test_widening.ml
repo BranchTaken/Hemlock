8a089552700d36a0b71ee6b642d4f334e7751559
Jason Evans <jasone@canonware.com>
2021-11-14 19:09:16 -0800

    Refactor Nat tests to use Fmt
    Resolves #189.

--- bootstrap/test/basis/n/test_widening.ml
+++ bootstrap/test/basis/n/test_widening.ml
@@ -1,22 +1,50 @@
 open! Basis.Rudiments
 open! Basis
 open N
-open Format
 
 let test () =
   let u64_max = of_u64 U64.max_value in
   let fifteen = of_string "15" in
-  printf "u64_max -> %a\n" xpp_x u64_max;
+  File.Fmt.stdout
+  |> Fmt.fmt "u64_max -> "
+  |> fmt ~alt:true ~base:Fmt.Hex ~pretty:true u64_max
+  |> Fmt.fmt "\n"
+  |> ignore;
 
   let r = (u64_max + one) in
-  printf "u64_max + %a -> %a %a\n" xpp one xpp_x r xpp r;
+  File.Fmt.stdout
+  |> Fmt.fmt "u64_max + "
+  |> pp one
+  |> Fmt.fmt " -> "
+  |> fmt ~alt:true ~base:Fmt.Hex ~pretty:true r
+  |> Fmt.fmt " "
+  |> pp r
+  |> Fmt.fmt "\n"
+  |> ignore;
 
   (* This underflow unintuitively results in two words rather than one, because it doesn't make
    * sense to trim leading "sign" bits for an arbitrary-precision unsigned result. *)
   let r = (zero - one) in
-  printf "%a - %a -> %a %a\n" xpp zero xpp one xpp_x r xpp r;
+  File.Fmt.stdout
+  |> pp zero
+  |> Fmt.fmt " - "
+  |> pp one
+  |> Fmt.fmt " -> "
+  |> fmt ~alt:true ~base:Fmt.Hex ~pretty:true r
+  |> Fmt.fmt " "
+  |> pp r
+  |> Fmt.fmt "\n"
+  |> ignore;
 
   let r = (u64_max * fifteen) in
-  printf "u64_max * %a -> %a %a\n" xpp fifteen xpp_x r xpp r
+  File.Fmt.stdout
+  |> Fmt.fmt "u64_max * "
+  |> pp fifteen
+  |> Fmt.fmt " -> "
+  |> fmt ~alt:true ~base:Fmt.Hex ~pretty:true r
+  |> Fmt.fmt " "
+  |> pp r
+  |> Fmt.fmt "\n"
+  |> ignore
 
 let _ = test ()
