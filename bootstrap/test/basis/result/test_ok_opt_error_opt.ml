7a7e27120a73d931099f1391d2aeae7d19195770
Jason Evans <jasone@canonware.com>
2021-11-16 14:44:55 -0800

    Refactor Result tests to use Fmt
    Resolves #193.

--- bootstrap/test/basis/result/test_ok_opt_error_opt.ml
+++ bootstrap/test/basis/result/test_ok_opt_error_opt.ml
@@ -1,18 +1,20 @@
 open! Basis.Rudiments
 open! Basis
 open Result
-open Format
 
 let test () =
-  printf "@[<h>";
   List.iter [Ok "ok"; Error "error"] ~f:(fun result ->
-    printf "ok_opt %a -> %a\n"
-      (xpp String.xpp String.xpp) result
-      (Option.xpp String.xpp) (ok_opt result);
-    printf "error_opt %a -> %a\n"
-      (xpp String.xpp String.xpp) result
-      (Option.xpp String.xpp) (error_opt result);
-  );
-  printf "@]"
+    File.Fmt.stdout
+    |> Fmt.fmt "ok_opt "
+    |> (pp String.pp String.pp) result
+    |> Fmt.fmt " -> "
+    |> (Option.fmt String.pp) (ok_opt result)
+    |> Fmt.fmt "\nerror_opt "
+    |> (pp String.pp String.pp) result
+    |> Fmt.fmt " -> "
+    |> (Option.fmt String.pp) (error_opt result)
+    |> Fmt.fmt "\n"
+    |> ignore
+  )
 
 let _ = test ()
