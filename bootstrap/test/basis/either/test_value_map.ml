f69e1dacc5dff69570064a5c77068424b54eda4e
Jason Evans <jasone@canonware.com>
2021-11-16 14:44:42 -0800

    Refactor Either tests to use Fmt
    Resolves #181.

--- bootstrap/test/basis/either/test_value_map.ml
+++ bootstrap/test/basis/either/test_value_map.ml
@@ -1,16 +1,18 @@
 open! Basis.Rudiments
 open! Basis
 open Either
-open Format
 
 let test () =
-  printf "@[<h>";
   let rec fn = function
     | [] -> ()
     | either :: eithers' -> begin
-        printf "value_map %a -> %a@\n"
-          (xpp Uns.xpp Uns.xpp) either Uns.xpp
-          (value_map ~first:(fun x -> x + 2L) ~second:(fun x -> x + 4L) either);
+        File.Fmt.stdout
+        |> Fmt.fmt "value_map "
+        |> (pp Uns.pp Uns.pp) either
+        |> Fmt.fmt " -> "
+        |> Uns.pp (value_map ~first:(fun x -> x + 2L) ~second:(fun x -> x + 4L) either)
+        |> Fmt.fmt "\n"
+        |> ignore;
         fn eithers'
       end
   in
@@ -18,7 +20,6 @@ let test () =
     First 1L;
     Second 2L;
   ] in
-  fn eithers;
-  printf "@]"
+  fn eithers
 
 let _ = test ()
