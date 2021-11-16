eeb6c8dba881681dc87b8f13f4196ecdb15e3175
Jason Evans <jasone@canonware.com>
2021-11-15 16:24:45 -0800

    Refactor Set tests to use Fmt
    Resolves #196.

--- bootstrap/test/basis/set/test_empty_cmper_m_singleton_length.ml
+++ bootstrap/test/basis/set/test_empty_cmper_m_singleton_length.ml
@@ -2,17 +2,20 @@ open! Basis.Rudiments
 open! Basis
 open SetTest
 open Set
-open Format
 
 let test () =
-  printf "@[";
   let e = empty (module Uns) in
   assert (length e = 0L);
-  printf "%a@\n" xpp e;
+  File.Fmt.stdout
+  |> fmt e
+  |> Fmt.fmt "\n"
+  |> ignore;
 
   let s = singleton (cmper_m e) 0L in
   assert (length s = 1L);
-  printf "%a@\n" xpp s;
-  printf "@]"
+  File.Fmt.stdout
+  |> fmt s
+  |> Fmt.fmt "\n"
+  |> ignore
 
 let _ = test ()
