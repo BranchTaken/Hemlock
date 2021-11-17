7326a8848413138c1cabeaa9c06cdc8f81711930
Jason Evans <jasone@canonware.com>
2021-11-16 17:20:04 -0800

    Refactor Q tests to use Fmt
    Resolves #202.

--- bootstrap/test/basis/q/test_is_empty.ml
+++ bootstrap/test/basis/q/test_is_empty.ml
@@ -1,17 +1,20 @@
 open! Basis.Rudiments
 open! Basis
 open Q
-open Format
 
 let test () =
-  let xppt = (xpp Uns.xpp) in
-  printf "@[<h>";
   let rec fn i n t = begin
     match i <= n with
     | false -> ()
     | true -> begin
         let e = is_empty t in
-        printf "is_empty %a = %b\n" xppt t e;
+        File.Fmt.stdout
+        |> Fmt.fmt "is_empty "
+        |> (pp Uns.pp) t
+        |> Fmt.fmt " = "
+        |> Bool.pp e
+        |> Fmt.fmt "\n"
+        |> ignore;
         fn (succ i) n (push_back i t)
       end
   end in
