ad39963e710f48e67c3b5377c24227440e50897d
Jason Evans <jasone@canonware.com>
2021-11-17 15:58:17 -0800

    Refactor U8 tests to use Fmt
    Resolves #210.

--- bootstrap/test/basis/u8/test_pp.ml
+++ bootstrap/test/basis/u8/test_pp.ml
@@ -1,15 +1,22 @@
 open! Basis.Rudiments
 open! Basis
 open U8
-open Format
 
 let test () =
-  printf "@[<h>";
   let rec print_xs xs = begin
     match xs with
     | [] -> ()
     | x :: xs' -> begin
-        printf "%a, %a, %a, %a\n" xpp_b x xpp_o x xpp x xpp_x x;
+        File.Fmt.stdout
+        |> fmt ~alt:true ~zpad:true ~width:8L ~base:Fmt.Bin ~pretty:true x
+        |> Fmt.fmt ", "
+        |> fmt ~alt:true ~zpad:true ~width:3L ~base:Fmt.Oct ~pretty:true x
+        |> Fmt.fmt ", "
+        |> pp x
+        |> Fmt.fmt ", "
+        |> fmt ~alt:true ~zpad:true ~width:2L ~base:Fmt.Hex ~pretty:true x
+        |> Fmt.fmt "\n"
+        |> ignore;
         print_xs xs'
       end
   end in
@@ -17,20 +24,22 @@ let test () =
     min_value;
     kv 42L;
     max_value;
-  ];
-  printf "@]"
+  ]
 
 let test2 () =
   let rec fn = function
     | [] -> ()
     | x :: xs' -> begin
-        printf "%a %a\n" xpp x xpp_x x;
+        File.Fmt.stdout
+        |> pp x
+        |> Fmt.fmt " "
+        |> fmt ~alt:true ~zpad:true ~width:2L ~base:Fmt.Hex ~pretty:true x
+        |> Fmt.fmt "\n"
+        |> ignore;
         fn xs'
       end
   in
-  printf "@[<h>";
-  fn [kv 0L; kv 1L; kv 42L; kv 255L];
-  printf "@]"
+  fn [kv 0L; kv 1L; kv 42L; kv 255L]
 
 let _ = test ()
 let _ = test2 ()
