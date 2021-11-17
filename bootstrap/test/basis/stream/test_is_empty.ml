96562001cc8b8f9bd1f278631f0c8bad6a0ea913
Jason Evans <jasone@canonware.com>
2021-11-16 16:59:17 -0800

    Refactor Stream tests to use Fmt
    Resolves #201.

--- bootstrap/test/basis/stream/test_is_empty.ml
+++ bootstrap/test/basis/stream/test_is_empty.ml
@@ -1,18 +1,21 @@
 open! Basis.Rudiments
 open! Basis
 open Stream
-open Format
 
 let test () =
-  let xppt = (xpp Uns.xpp) in
-  printf "@[<h>";
   let rec test_is_empty_up_to i n = begin
     match i <= n with
     | false -> ()
     | true -> begin
         let t = init (0L =:< i) ~f:(fun i -> i) in
         let e = is_empty t in
-        printf "is_empty %a = %b\n" xppt t e;
+        File.Fmt.stdout
+        |> Fmt.fmt "is_empty "
+        |> (pp Uns.pp) t
+        |> Fmt.fmt " = "
+        |> Bool.pp e
+        |> Fmt.fmt "\n"
+        |> ignore;
         test_is_empty_up_to (succ i) n
       end
   end in
--- bootstrap/test/basis/stream/test_length.ml
+++ bootstrap/test/basis/stream/test_length.ml
@@ -1,18 +1,21 @@
 open! Basis.Rudiments
 open! Basis
 open Stream
-open Format
 
 let test () =
-  let xppt = (xpp Uns.xpp) in
-  printf "@[<h>";
   let rec test_length_up_to i n = begin
     match i <= n with
     | false -> ()
     | true -> begin
         let t = init (0L =:< i) ~f:(fun i -> i) in
         let l = length t in
-        printf "length %a = %a\n" xppt t Uns.xpp l;
+        File.Fmt.stdout
+        |> Fmt.fmt "length "
+        |> (pp Uns.pp) t
+        |> Fmt.fmt " = "
+        |> Uns.pp l
+        |> Fmt.fmt "\n"
+        |> ignore;
         test_length_up_to (succ i) n
       end
   end in
