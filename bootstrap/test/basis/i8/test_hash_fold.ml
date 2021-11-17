c8a3af0a310f725871763bb365fe3762318b657a
Jason Evans <jasone@canonware.com>
2021-11-17 11:54:27 -0800

    Refactor I8 tests to use Fmt
    Resolves #205.

--- bootstrap/test/basis/i8/test_hash_fold.ml
+++ bootstrap/test/basis/i8/test_hash_fold.ml
@@ -1,21 +1,23 @@
 open! Basis.Rudiments
 open! Basis
 open I8
-open Format
 
 let test () =
-  printf "@[<h>";
   let rec test_hash_fold xs = begin
     match xs with
     | [] -> ()
     | x :: xs' -> begin
-        printf "hash_fold %a -> %a\n"
-          xpp_x x Hash.xpp (Hash.t_of_state (hash_fold x Hash.State.empty));
+        File.Fmt.stdout
+        |> Fmt.fmt "hash_fold "
+        |> fmt ~alt:true ~zpad:true ~width:2L ~base:Fmt.Hex ~pretty:true x
+        |> Fmt.fmt " -> "
+        |> Hash.pp (Hash.t_of_state (hash_fold x Hash.State.empty))
+        |> Fmt.fmt "\n"
+        |> ignore;
         test_hash_fold xs'
       end
   end in
   let xs = [min_value; neg_one; zero; one; max_value] in
-  test_hash_fold xs;
-  printf "@]"
+  test_hash_fold xs
 
 let _ = test ()
