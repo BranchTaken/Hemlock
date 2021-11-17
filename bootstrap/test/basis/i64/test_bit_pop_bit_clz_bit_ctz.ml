2000e6047522ca8fec21eef274c33ed9baf9691e
Jason Evans <jasone@canonware.com>
2021-11-17 14:15:39 -0800

    Refactor I64 tests to use Fmt
    Resolves #208.

--- bootstrap/test/basis/i64/test_bit_pop_bit_clz_bit_ctz.ml
+++ bootstrap/test/basis/i64/test_bit_pop_bit_clz_bit_ctz.ml
@@ -1,16 +1,23 @@
 open! Basis.Rudiments
 open! Basis
 open I64
-open Format
 
 let test () =
-  printf "@[<h>";
-  let rec test_u64s = function
+  let rec test_i64s = function
     | [] -> ()
-    | u :: us' -> begin
-        printf "bit_{pop,clz,ctz} %a -> %Lu, %Lu, %Lu\n"
-          xpp_x u (bit_pop u) (bit_clz u) (bit_ctz u);
-        test_u64s us'
+    | x :: xs' -> begin
+        File.Fmt.stdout
+        |> Fmt.fmt "bit_{pop,clz,ctz} "
+        |> fmt ~alt:true ~zpad:true ~width:16L ~base:Fmt.Hex ~pretty:true x
+        |> Fmt.fmt " -> "
+        |> Uns.pp (bit_pop x)
+        |> Fmt.fmt ", "
+        |> Uns.pp (bit_clz x)
+        |> Fmt.fmt ", "
+        |> Uns.pp (bit_ctz x)
+        |> Fmt.fmt "\n"
+        |> ignore;
+        test_i64s xs'
       end
   in
   let us = [
@@ -19,7 +26,6 @@ let test () =
     of_string "0x8000_0000_0000_0000";
     of_string "0xffff_ffff_ffff_ffff"
   ] in
-  test_u64s us;
-  printf "@]"
+  test_i64s us
 
 let _ = test ()
