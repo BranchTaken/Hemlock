53c56b8580a34436306205e994ed97c1c9120fb2
Jason Evans <jasone@canonware.com>
2021-11-16 17:27:05 -0800

    Refactor Deq tests to use Fmt
    Resolves #203.

--- bootstrap/test/basis/deq/test_back.ml
+++ bootstrap/test/basis/deq/test_back.ml
@@ -1,22 +1,24 @@
 open! Basis.Rudiments
 open! Basis
 open Deq
-open Format
 
 let test () =
-  let xppt = (xpp Uns.xpp) in
-  printf "@[<h>";
   let rec fn i n t = begin
     match i <= n with
     | false -> ()
     | true -> begin
         let t' = push i t in
         let elm = back t' in
-        printf "back %a = %a\n" xppt t' Uns.xpp elm;
+        File.Fmt.stdout
+        |> Fmt.fmt "back "
+        |> (pp Uns.pp) t'
+        |> Fmt.fmt " = "
+        |> Uns.pp elm
+        |> Fmt.fmt "\n"
+        |> ignore;
         fn (succ i) n t'
       end
   end in
-  fn 0L 4L empty;
-  printf "@]"
+  fn 0L 4L empty
 
 let _ = test ()
