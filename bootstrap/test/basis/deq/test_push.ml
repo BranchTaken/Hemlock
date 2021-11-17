499f38ddd515a28b2fb7fdd6f05bbd809a03dbc8
Jason Evans <jasone@canonware.com>
2021-11-16 17:27:05 -0800

    Refactor Deq tests to use Fmt
    Resolves #203.

--- bootstrap/test/basis/deq/test_push.ml
+++ bootstrap/test/basis/deq/test_push.ml
@@ -1,21 +1,25 @@
 open! Basis.Rudiments
 open! Basis
 open Deq
-open Format
 
 let test () =
-  let xppt = (xpp Uns.xpp) in
-  printf "@[<h>";
   let rec fn i n t = begin
     match i <= n with
     | false -> ()
     | true -> begin
         let t' = push i t in
-        printf "push %a %a = %a\n" Uns.xpp i xppt t xppt t';
+        File.Fmt.stdout
+        |> Fmt.fmt "push "
+        |> Uns.pp i
+        |> Fmt.fmt " "
+        |> (pp Uns.pp) t
+        |> Fmt.fmt " = "
+        |> (pp Uns.pp) t'
+        |> Fmt.fmt "\n"
+        |> ignore;
         fn (succ i) n t'
       end
   end in
-  fn 0L 4L empty;
-  printf "@]"
+  fn 0L 4L empty
 
 let _ = test ()
