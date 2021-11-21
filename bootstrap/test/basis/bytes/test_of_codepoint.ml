f39dfdd7b487eac4da16b564e23ed8ac117bd09a
Jason Evans <jasone@canonware.com>
2021-11-20 19:56:24 -0800

    Refactor Bytes tests to use Fmt
    Resolves #176.

--- bootstrap/test/basis/bytes/test_of_codepoint.ml
+++ bootstrap/test/basis/bytes/test_of_codepoint.ml
@@ -1,7 +1,6 @@
 open! Basis.Rudiments
 open! Basis
 open Bytes
-open Format
 
 let test () =
   let strs = [
@@ -13,14 +12,16 @@ let test () =
   let cps = List.fold_right strs ~init:[] ~f:(fun s cps ->
     String.C.Cursor.(rget (hd s)) :: cps
   ) in
-  printf "@[<h>";
   List.iter cps ~f:(fun cp ->
     let bytes = of_codepoint cp in
-    printf "'%s' -> %a -> %a\n"
-      (String.of_codepoint cp)
-      xpp bytes
-      String.xpp (to_string_hlt bytes)
-  );
-  printf "@]"
+    File.Fmt.stdout
+    |> Codepoint.pp cp
+    |> Fmt.fmt " -> "
+    |> pp bytes
+    |> Fmt.fmt " -> "
+    |> String.pp (to_string_hlt bytes)
+    |> Fmt.fmt "\n"
+    |> ignore
+  )
 
 let _ = test ()
