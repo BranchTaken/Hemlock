bb49946556ef90fee8d04714d1924e459b237d69
Cameron Evans <c.d.evans87@gmail.com>
2022-01-06 07:46:02 +0000

    Add File.Open module.

---
+++ bootstrap/test/basis/file/test_file_open.ml
@@ -0,0 +1,14 @@
+open Basis
+
+external user_data_pp: File.t -> File.Open.t -> unit = "hm_basis_file_user_data_pp"
+external sqring_pp: File.t -> unit = "hm_basis_file_sqring_pp"
+
+let () =
+  let s = File.Open.submit_hlt ~flag:File.Flag.RW (Bytes.Slice.of_string_slice
+      (String.C.Slice.of_string "file")) in
+  user_data_pp File.stdout s;
+  sqring_pp File.stdout;
+  let _ = File.Open.complete_hlt s in
+  user_data_pp File.stdout s;
+  sqring_pp File.stdout;
+  ()
