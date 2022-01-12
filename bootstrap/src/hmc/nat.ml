60c94d324ac97188ea275f6b651ca0b7358e8926
Jason Evans <je@branchtaken.com>
2022-01-12 13:55:32 -0800

    Increase Nat/Zint limits
    Increase limits such that memory exhaustion is the only practical limit on how
    large values can be.

--- bootstrap/src/hmc/nat.ml
+++ bootstrap/src/hmc/nat.ml
@@ -4,7 +4,7 @@ open Basis.Rudiments
 module T = struct
   type t = u64 array
   let min_word_length = 0L
-  let max_word_length = Uns.(2L ** 26L) (* 2**32 bits. *)
+  let max_word_length = Uns.(2L ** 58L) (* 2**64 bits. *)
   let init n ~f =
     Array.init (0L =:< n) ~f
   let word_length = Array.length
