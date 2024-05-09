c06ebd2e91706a68e4f537b9f937cce253aa6816
Jason Evans <je@branchtaken.com>
2024-05-08 21:24:44 -0700

    Flatten Attribs.t

--- bootstrap/bin/hocc/attribs.ml
+++ bootstrap/bin/hocc/attribs.ml
@@ -32,57 +32,48 @@ module K = struct
 end
 
 module T = struct
-  type t = {
-    symbol_attribs: (K.t, Attrib.t, K.cmper_witness) Ordmap.t (* XXX Flatten. *)
-  }
+  type t = (K.t, Attrib.t, K.cmper_witness) Ordmap.t
 
-  let hash_fold {symbol_attribs; _} =
-    Ordmap.hash_fold (fun attrib state -> state |> Attrib.hash_fold attrib) symbol_attribs
+  let hash_fold t =
+    Ordmap.hash_fold (fun attrib state -> state |> Attrib.hash_fold attrib) t
 
-  let cmp {symbol_attribs=sa0; _} {symbol_attribs=sa1; _} =
-    Ordmap.cmp (fun attrib0 attrib1 -> Attrib.cmp attrib0 attrib1) sa0 sa1
+  let cmp t0 t1 =
+    Ordmap.cmp (fun attrib0 attrib1 -> Attrib.cmp attrib0 attrib1) t0 t1
 
-  let fmt ?(alt=false) ?(width=0L) {symbol_attribs} formatter =
+  let fmt ?(alt=false) ?(width=0L) t formatter =
     formatter
-    |> Fmt.fmt "{symbol_attribs="
     |> Ordmap.fmt ~alt ~width (fun attrib formatter ->
       formatter |> Attrib.pp attrib
-    ) symbol_attribs
-    |> Fmt.fmt "}"
+    ) t
 
-  let pp {symbol_attribs} formatter =
+  let pp t formatter =
     formatter
-    |> Fmt.fmt "{symbol_attribs="
-    |> Ordmap.pp (fun attrib formatter -> formatter |> Attrib.pp attrib) symbol_attribs
-    |> Fmt.fmt "}"
+    |> Ordmap.pp (fun attrib formatter -> formatter |> Attrib.pp attrib) t
 
-  let fmt_hr symbols prods ?(alt=false) ?(width=0L) {symbol_attribs}
+  let fmt_hr symbols prods ?(alt=false) ?(width=0L) t
     formatter =
     let attrib_lst = Ordmap.fold_right ~init:[]
-      ~f:(fun attrib_lst (_, attrib) -> attrib :: attrib_lst) symbol_attribs in
+      ~f:(fun attrib_lst (_, attrib) -> attrib :: attrib_lst) t in
     formatter
-    |> Fmt.fmt "{symbol_attribs="
     |> (fun formatter ->
       List.fmt ~alt ~width (Attrib.fmt_hr symbols prods ~alt ~width) attrib_lst formatter
     )
-    |> Fmt.fmt "}"
 end
 include T
 include Identifiable.Make(T)
 
-let length {symbol_attribs} =
-  Ordmap.length symbol_attribs
+let length =
+  Ordmap.length
 
-let equal {symbol_attribs=sa0} {symbol_attribs=sa1} =
-  Ordmap.equal (fun attrib0 attrib1 -> Attrib.equal attrib0 attrib1) sa0 sa1
+let equal t0 t1 =
+  Ordmap.equal (fun attrib0 attrib1 -> Attrib.equal attrib0 attrib1) t0 t1
 
 module Seq = struct
   type container = t
   type elm = Attrib.t
   type t = (K.t, Attrib.t, K.cmper_witness) Ordmap.Seq.t
 
-  let init {symbol_attribs} =
-    Ordmap.Seq.init symbol_attribs
+  let init = Ordmap.Seq.init
   let length = Ordmap.Seq.length
   let next seq =
     match Ordmap.Seq.next seq with
@@ -93,29 +84,25 @@ module Seq = struct
     | Some ((_symbol_index, attrib), seq') -> Some (attrib, seq')
 end
 
-let empty =
-  {symbol_attribs=Ordmap.empty (module K)}
+let empty = Ordmap.empty (module K)
 
 let singleton (Attrib.{conflict_state_index; symbol_index; _} as attrib) =
   let k = K.{conflict_state_index; symbol_index} in
-  {
-    symbol_attribs=Ordmap.singleton (module K) ~k ~v:attrib
-  }
+  Ordmap.singleton (module K) ~k ~v:attrib
 
-let is_empty {symbol_attribs} =
-  Ordmap.is_empty symbol_attribs
+let is_empty = Ordmap.is_empty
 
-let get ~conflict_state_index symbol_index {symbol_attribs} =
+let get ~conflict_state_index symbol_index t =
   let k = K.{conflict_state_index; symbol_index} in
-  Ordmap.get k symbol_attribs
+  Ordmap.get k t
 
-let get_hlt ~conflict_state_index symbol_index {symbol_attribs} =
+let get_hlt ~conflict_state_index symbol_index t =
   let k = K.{conflict_state_index; symbol_index} in
-  Ordmap.get_hlt k symbol_attribs
+  Ordmap.get_hlt k t
 
-let amend ~conflict_state_index symbol_index ~f {symbol_attribs} =
+let amend ~conflict_state_index symbol_index ~f t =
   let k = K.{conflict_state_index; symbol_index} in
-  let symbol_attribs' = Ordmap.amend k ~f:(fun attrib_opt ->
+  Ordmap.amend k ~f:(fun attrib_opt ->
     let attrib_opt' = f attrib_opt in
     let () = match attrib_opt, attrib_opt' with
       | Some attrib, Some attrib' -> assert (Attrib.equal_keys attrib attrib');
@@ -124,8 +111,7 @@ let amend ~conflict_state_index symbol_index ~f {symbol_attribs} =
       | None, None -> ()
     in
     attrib_opt'
-  ) symbol_attribs in
-  {symbol_attribs=symbol_attribs'}
+  ) t
 
 let insert (Attrib.{conflict_state_index; symbol_index; _} as attrib) t =
   amend ~conflict_state_index symbol_index ~f:(function
@@ -136,29 +122,28 @@ let insert (Attrib.{conflict_state_index; symbol_index; _} as attrib) t =
       end
   ) t
 
-let union {symbol_attribs=sa0} {symbol_attribs=sa1} =
-  let symbol_attribs = Ordmap.union ~f:(fun _k attrib0 attrib1 ->
+let union t0 t1 =
+  Ordmap.union ~f:(fun _k attrib0 attrib1 ->
     assert (Attrib.equal_keys attrib0 attrib1);
     Attrib.union attrib0 attrib1
-  ) sa0 sa1 in
-  {symbol_attribs}
+  ) t0 t1
 
-let reindex index_map {symbol_attribs} =
+let reindex index_map t =
   Ordmap.fold ~init:empty
     ~f:(fun reindexed_t (_symbol_index, (Attrib.{conflict_state_index; _} as attrib)) ->
       match Map.get conflict_state_index index_map with
       | None -> reindexed_t
       | Some conflict_state_index' ->
         insert {attrib with conflict_state_index=conflict_state_index'} reindexed_t
-    ) symbol_attribs
+    ) t
 
-let fold_until ~init ~f {symbol_attribs} =
-  Ordmap.fold_until ~init ~f:(fun accum (_symbol_index, attrib) -> f accum attrib) symbol_attribs
+let fold_until ~init ~f t =
+  Ordmap.fold_until ~init ~f:(fun accum (_symbol_index, attrib) -> f accum attrib) t
 
-let fold ~init ~f {symbol_attribs} =
-  Ordmap.fold ~init ~f:(fun accum (_symbol_index, attrib) -> f accum attrib) symbol_attribs
+let fold ~init ~f t =
+  Ordmap.fold ~init ~f:(fun accum (_symbol_index, attrib) -> f accum attrib) t
 
-let fold2_until ~init ~f {symbol_attribs=sa0} {symbol_attribs=sa1} =
+let fold2_until ~init ~f t0 t1 =
   Ordmap.fold2_until ~init ~f:(fun accum k_kv_opt0 k_kv_opt1 ->
     let kv_opt0 = match k_kv_opt0 with
       | None -> None
@@ -169,9 +154,9 @@ let fold2_until ~init ~f {symbol_attribs=sa0} {symbol_attribs=sa1} =
       | Some (_k, attrib) -> Some attrib
     in
     f accum kv_opt0 kv_opt1
-  ) sa0 sa1
+  ) t0 t1
 
-let fold2 ~init ~f {symbol_attribs=sa0} {symbol_attribs=sa1} =
+let fold2 ~init ~f t0 t1 =
   Ordmap.fold2 ~init ~f:(fun accum k_kv_opt0 k_kv_opt1 ->
     let kv_opt0 = match k_kv_opt0 with
       | None -> None
@@ -182,4 +167,4 @@ let fold2 ~init ~f {symbol_attribs=sa0} {symbol_attribs=sa1} =
       | Some (_k, attrib) -> Some attrib
     in
     f accum kv_opt0 kv_opt1
-  ) sa0 sa1
+  ) t0 t1
