BranchTaken/Hemlock

commit 28e65408eb50e419ddf2e29411a9fe1a2269a78b

Optimize `Bitset.fold`
OCaml-based `Nat` implementation performance is very sensitive to allocations.
Refactor `Bitset.next_ge_opt` to allocate one `Nat` rather than three. This
implementation is slightly less obvious, and wouldn't make much difference were
`Nat` contiguously allocated.

bootstrap/src/basis/bitset.ml
@@ -45,11 +45,9 @@ module T = struct
     | None -> halt "No last element"
 
   let next_ge_opt elm t =
-    let mask = Nat.pred (singleton elm) in
-    let bitset_lt = Nat.bit_and mask t in
-    let bitset_ge = Nat.bit_xor bitset_lt t in
-    match is_empty bitset_ge with
-    | false -> Some (Nat.bit_ctz bitset_ge)
+    let bitset_sr_ge = Nat.bit_sr ~shift:elm t in
+    match is_empty bitset_sr_ge with
+    | false -> Some ((Nat.bit_ctz bitset_sr_ge) + elm)
     | true -> None
 
   let next_ge elm t =

commit b9102d9ec13813a03a959187bfe07656299b1d69

Fix regression in `Bitset.fold`
Avoid bit shifting by amounts greater than or equal to the underlying nat's
bitwidth. This regression was caused by 28e65408eb50e419ddf2e29411a9fe1a2269a78b
(Optimize `Bitset.fold`).

bootstrap/src/basis/bitset.ml
@@ -45,9 +45,13 @@ module T = struct
     | None -> halt "No last element"
 
   let next_ge_opt elm t =
-    let bitset_sr_ge = Nat.bit_sr ~shift:elm t in
-    match is_empty bitset_sr_ge with
-    | false -> Some ((Nat.bit_ctz bitset_sr_ge) + elm)
+    match elm >= Nat.bit_length t with
+    | false -> begin
+        let bitset_sr_ge = Nat.bit_sr ~shift:elm t in
+        match is_empty bitset_sr_ge with
+        | false -> Some ((Nat.bit_ctz bitset_sr_ge) + elm)
+        | true -> None
+      end
     | true -> None
 
   let next_ge elm t =

bootstrap/test/basis/bitset/dune
@@ -5,6 +5,7 @@
   test_empty_singleton_length
   test_filter
   test_filteri
+  test_fold
   test_fold_right_until
   test_fold_until
   test_fold2

bootstrap/test/basis/bitset/test_fold.expected
@@ -1,0 +1,21 @@
+bitset=[]
+     ->
+     ->
+bitset=[0]
+     -> 0
+     -> 0
+bitset=[0; 1]
+     -> 0 1
+     -> 1 0
+bitset=[0; 1; 2]
+     -> 0 1 2
+     -> 2 1 0
+bitset=[0; 1; 62; 63]
+     -> 0 1 62 63
+     -> 63 62 1 0
+bitset=[0; 1; 66]
+     -> 0 1 66
+     -> 66 1 0
+bitset=[0; 1; 66; 91]
+     -> 0 1 66 91
+     -> 91 66 1 0

bootstrap/test/basis/bitset/test_fold.ml
@@ -1,0 +1,36 @@
+open! Basis.Rudiments
+open! Basis
+open Bitset
+
+let test () =
+  let test ms = begin
+    let bitset = of_list ms in
+    File.Fmt.stdout
+    |> Fmt.fmt "bitset=" |> Bitset.pp bitset
+    |> Fmt.fmt "\n     ->" |> ignore;
+    let () = fold ~init:() ~f:(fun _accum elm ->
+      File.Fmt.stdout
+      |> Fmt.fmt " " |> Uns.pp elm |> ignore
+    ) bitset in
+    File.Fmt.stdout
+    |> Fmt.fmt "\n     ->" |> ignore;
+    let () = fold_right ~init:() ~f:(fun _accum elm ->
+      File.Fmt.stdout
+      |> Fmt.fmt " " |> Uns.pp elm |> ignore
+    ) bitset in
+    File.Fmt.stdout |> Fmt.fmt "\n" |> ignore
+  end in
+  let test_lists = [
+    [];
+    [0L];
+    [0L; 1L];
+    [0L; 1L; 2L];
+    [0L; 1L; 62L; 63L];
+    [0L; 1L; 66L];
+    [0L; 1L; 66L; 91L];
+  ] in
+  List.iter test_lists ~f:(fun ms ->
+    test ms
+  )
+
+let _ = test ()