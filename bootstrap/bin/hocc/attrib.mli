0f4fe82fffe0229038656001132c7f60925e03d6
Jason Evans <je@branchtaken.com>
2026-04-02 11:13:47 -0700

    Remove unused `Attrib.equal_ielr`

--- bootstrap/bin/hocc/attrib.mli
+++ bootstrap/bin/hocc/attrib.mli
@@ -70,10 +70,6 @@ val inter: t -> t -> t
 val diff: t -> t -> t
 (** [diff t0 t1] returns an attrib containing the contents of [t0] that are not in [t1]. *)
 
-val equal_ielr: resolve:bool -> Symbols.t -> Prods.t -> t -> t -> bool
-(** [equal_ielr ~resolve symbols prods t0 t1] determines whether [t0] and [t1] make equal
-    contributions. *)
-
 val compat_ielr: resolve:bool -> Symbols.t -> Prods.t -> t -> t -> bool
 (** [compat_ielr ~resolve symbols prods t0 t1] determines whether [t0] and [t1] make compatible
     contributions. If [resolve] is true, allow conflicts that cannot lead to inadequacy (i.e.
